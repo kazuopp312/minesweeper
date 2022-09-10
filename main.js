2260d8d84e66f6f22c674169174fb1ed2c8a7ac0
kazuopp312 <kazuopp312@gmail.com>
2022-09-10 09:15:09 +0900

    start making minesweeper

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const grid = document.querySelector('.grid');
+  let width = 10;
+  let squares = [];
+
+  // create board
+  function createBoard() {
+    for(let i = 0; i < width * width; i++) {
+      const square = document.createElement('div');
+      square.setAttribute('id', i);
+      grid.appendChild(square);
+      squares.push(square);
+    }
+  }
+  createBoard();
+
+})
